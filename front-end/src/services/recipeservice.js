8b8e5ad0793a002d0549aea6e6acbf254704352d
Atharve Pandey <pandey.atharve@gmail.com>
2024-10-29 15:00:34 -0400

    wait this is the file i added 2 new functions to, the other one is just comments

--- front-end/src/services/recipeservice.js
+++ front-end/src/services/recipeservice.js
@@ -1,6 +1,12 @@
 export default class RecipeService {
     constructor(recipes){
         this.recipes = recipes;
+        let a = [];
+    }
+
+
+    addRecipes(recipe){
+        this.recipes.push(recipe)
     }
 
     getRecipes(){
@@ -9,5 +15,9 @@ export default class RecipeService {
 
     getRecipeById(id){
         return this.recipes.find(recipe => recipe.id === parseInt(id));
+    }
+
+    getLength(){
+        return this.recipes.length;
     }
 }
