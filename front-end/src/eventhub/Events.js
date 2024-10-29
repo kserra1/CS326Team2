e068d1d1d9fd086da3d8f8ef2e17b01ac02215d7
Atharve Pandey <pandey.atharve@gmail.com>
2024-10-29 12:58:41 -0400

    added more comments here

--- front-end/src/eventhub/Events.js
+++ front-end/src/eventhub/Events.js
@@ -3,3 +3,5 @@ import EventHub from "./EventHub";
 const eventHub = new EventHub();
 
 export default eventHub; //If other files need to use eventHub object, use this file (acts as a factory for eventhub object)
+
+//essentially prevents the need to instantiate a new object in other files, further decreasing load on memory...
