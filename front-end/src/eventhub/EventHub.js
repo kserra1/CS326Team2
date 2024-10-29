34d1b90b1e23257a6c0c8ba02d22a2d39d043822
Atharve Pandey <pandey.atharve@gmail.com>
2024-10-29 12:03:11 -0400

    changed and commented eventHub file

--- front-end/src/eventhub/EventHub.js
+++ front-end/src/eventhub/EventHub.js
@@ -1,18 +1,20 @@
 export default class EventHub {
     constructor() {
-        this.events = {};
+        this.events = {}; //init an empty hashmap
     }
 
     on(event, callback){
         if(!this.events[event]){
-            this.events[event] = [];
+            this.events[event] = []; //init empty array if event isn't already in hashmap (there exists an array for each event)
         }
-        this.events[event].push(callback);
+        this.events[event].push(callback); //push a callback that should be executed when that even occurs
     }
 
     emit(event, data){
         if(this.events[event]){
             this.events[event].forEach(callback => callback(data));
+        }else{
+            throw new Error("event not in EventHub"); //good to have if someone forgets to always call on method before emit method
         }
     }
 }
