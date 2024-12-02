545d329081d32efeb440bc0ce27ed0400e22216c
Kieran <kserra@umass.edu>
2024-12-02 18:54:51 -0500

    Authentication #60 Enabled JWT tokens to be created on backend and stored on frontend to allow for user sessions and protecting routes

--- back-end/src/routes/RecipeRoutes.js
+++ back-end/src/routes/RecipeRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import UserController from "../controller/UserController.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 //Contains routes for whole application
 class RecipeRoutes {
     constructor() {
@@ -8,10 +9,12 @@ class RecipeRoutes {
     }
     //Define the routes and connect them to controller methods
     initializeRoutes(){
-        this.router.get("/recipes", async (req, res) => {
+        this.router.get("/recipes", authenticateToken,  async (req, res) => {
             //Will have recipe controller but mock for now:
             await res.json({recipes: []});
         });
+        //Get profile info:
+        this.router.get("/profile", authenticateToken, UserController.getUserInfo.bind(UserController));
         //Register a new user
         this.router.post("/register", async (req, res)=>{
             await UserController.register(req, res);
